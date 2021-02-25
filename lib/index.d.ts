618f9a3d96e04618642d21a7ebfab2b52b72e5f6
benevolarX <benevolar.nouveau.project@gmail.com>
2021-02-25 14:19:19 +0100

    d.ts

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,29 +1,52 @@
+import { IncomingHttpHeaders } from 'http'
+interface Headers extends IncomingHttpHeaders {
+  [key: string]: string
+}
+
+interface Request extends IncomingHttpHeaders {
+  headers: Headers;
+  connection: {
+    remoteAddress?: string;
+    socket?: {
+      remoteAddress?: string
+    };
+  };
+  info?: {
+    remoteAddress?: string
+  };
+  socket?: {
+    remoteAddress?: string
+  };
+}
+
+interface Options {
+  headers?: string[]
+}
 
 /**
  * test if v is ipv4
  * @param {string} v
  */
-export function isIpv4(v: string): boolean;
+export declare function isIpv4(v: string): boolean;
 /**
  * test if v is ipv6
  * @param {string} v
  */
-export function isIpv6(v: string): boolean;
+export declare function isIpv6(v: string): boolean;
 /**
  * test if v is ip (v4 or v6)
  * @param {string} v
  */
-export function isIp(v: string): boolean;
+export declare function isIp(v: string): boolean;
 /**
  * get ip from request
- * @param {http.IncomingMessage} req
+ * @param {Request} req
  * @returns {string|null}
  */
-export function getIpFromRequest(req: any): string | null;
+export declare function getIpFromRequest(req: Request): string | null;
 /**
  * generate custom getIpFromRequest function
- * @param {*} options
- * @return {(req: http.IncomingMessage) => string|null}
+ * @param {Options} options
+ * @return {(req: Request) => string|null}
  */
-export function getIpFromRequestBuilder(options?: any): (req: any) => string | null;
-
+export declare function getIpFromRequestBuilder(options?: Options): (req: Request) => string | null;
